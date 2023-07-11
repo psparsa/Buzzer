7e234aa95b22df4d3ecf38d0c3a768e08814a0cd
PS-PARSA <57572461+psparsa@users.noreply.github.com>
2023-07-12 01:44:30 +0330

    add test cases

--- app/Buzzer.hs
+++ app/Buzzer.hs
@@ -1,2 +1,11 @@
 module Buzzer where
-    buzzer n = 2
+    buzzer :: Int -> Either String Int
+    buzzer n
+        | n < 3 = Right n
+        | mod n 105 == 0 = Left "FizzBuzzBizz"
+        | mod n 35 == 0 = Left "BuzzBizz"
+        | mod n 21 == 0 = Left "FizzBizz"
+        | mod n 15 == 0 = Left "FizzBuzz"
+        | mod n 3 == 0 = Left "Fizz"
+        | mod n 5 == 0 = Left "Buzz"
+        | mod n 7 == 0 = Left "Bizz"
--- app/Tests.hs
+++ app/Tests.hs
@@ -1,15 +1,84 @@
 import           Buzzer      (buzzer)
 import qualified System.Exit as Exit
 import           Test.HUnit  (Counts (failures), Test (..), assertEqual,
-                              runTestTT)
+                              assertFailure, runTestTT)
 
-testsBizz :: Test
-testsBizz = TestCase (assertEqual "Should return the same value:" 2 (buzzer 2))
+wrongTypeMsg :: String
+wrongTypeMsg = "Function returned the wrong type!"
+
+divTestMsg :: String -> String
+divTestMsg n = "Tests the case where the input number is divisible by " ++ n
+
+testLessThanThree :: Test
+testLessThanThree = TestCase $ do
+    let result = buzzer 2
+    case result of
+        Right number -> assertEqual "Tests with a number less than 3" 2 number
+        Left _       -> assertFailure wrongTypeMsg
+
+testThree :: Test
+testThree = TestCase $ do
+    let result = buzzer 3
+    case result of
+        Right _     -> assertFailure wrongTypeMsg
+        Left number -> assertEqual (divTestMsg "3") "Fizz" number
 
 
+testFive :: Test
+testFive = TestCase $ do
+    let result = buzzer 5
+    case result of
+        Right _     -> assertFailure wrongTypeMsg
+        Left number -> assertEqual (divTestMsg "5") "Buzz" number
+
+
+testSeven :: Test
+testSeven = TestCase $ do
+    let result = buzzer 7
+    case result of
+        Right _     -> assertFailure wrongTypeMsg
+        Left number -> assertEqual (divTestMsg "7") "Bizz" number
+
+
+testFifteen :: Test
+testFifteen = TestCase $ do
+    let result = buzzer 15
+    case result of
+        Right _     -> assertFailure wrongTypeMsg
+        Left number -> assertEqual (divTestMsg "3 and 5") "FizzBuzz" number
+
+testTwentyOne :: Test
+testTwentyOne = TestCase $ do
+    let result = buzzer 21
+    case result of
+        Right _     -> assertFailure wrongTypeMsg
+        Left number -> assertEqual (divTestMsg "3 and 7") "FizzBizz" number
+
+testThirtyFive :: Test
+testThirtyFive = TestCase $ do
+    let result = buzzer 35
+    case result of
+        Right _     -> assertFailure wrongTypeMsg
+        Left number -> assertEqual (divTestMsg "5 and 7") "BuzzBizz" number
+
+testOneHundredFiveThirtyFive :: Test
+testOneHundredFiveThirtyFive = TestCase $ do
+    let result = buzzer 105
+    case result of
+        Right _     -> assertFailure wrongTypeMsg
+        Left number -> assertEqual (divTestMsg "3, 5 and 7") "FizzBuzzBizz" number
 
 buzzerTests :: Test
-buzzerTests = TestList [testsBizz]
+buzzerTests = TestList [
+    testLessThanThree,
+    testThree,
+    testFive,
+    testSeven,
+    testFifteen,
+    testTwentyOne,
+    testThirtyFive,
+    testOneHundredFiveThirtyFive
+    ]
 
 main :: IO ()
 main = do
